953c234961926f38ebcddbe739c9d28aeb593e62
Wesley Huang <wesleyhuang@Wesleyde-MacBook-Air.local>
2022-05-20 00:32:05 +0800

    Factorial Number C++ program

---
+++ Factorial Number/Factorial Number/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Factorial Number
+//
+//  Created by Wesley Huang on 2022/5/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ FactorialNumber.cpp
@@ -0,0 +1,31 @@
+/* Project: C++ program to find factorial of given number*
+ *In mathematics, the factorial of a non-negative integer n,
+ *is the product of all positive integers less than or equal to n
+ *
+ *Author: Wesley Yu-Wei Huang
+ *School: Jianguo Senior High School
+ *Date: 11/11/2021
+ */
+#include <iostream>
+using namespace std;
+ 
+// function to find factorial of given number
+unsigned int factorial(unsigned int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+// Driver code
+/*
+* main: the program main function
+*/
+int main()
+{
+    int num ;
+    cin >> num;
+    cout << "Factorial of "
+         << num << " is " << factorial(num) << endl;
+    return 0;
+}
